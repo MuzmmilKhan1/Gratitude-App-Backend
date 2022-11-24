1875380edc4fd70060fb4a6ea7fea6bdd1392bc9
MuzmmilKhan1 <77438943+MuzmmilKhan1@users.noreply.github.com>
2022-11-24 22:43:35 +0000

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const MyModel = mongoose.model('gratitude', new gratitude({
  
 // Cors and Body Parser for Getting His typed project
 app.use(cors({
-    origin: 'https://gratitude-app-by-muzamil.web.app/dashboard',
+    origin: 'http://localhost:3000/dashboard',
     credentials: true
 }));
 app.use(bodyParser.urlencoded({ extended: true })); 
